ca9c771ba6b5976f393f027d5c736e3ecbf68659
fceja <cisco_ceja@icloud.com>
2023-10-12 16:41:35 -0700

    fix typo

--- src/ts/components/cardCarousel/CardCarouselCardData.ts
+++ src/ts/components/cardCarousel/CardCarouselCardData.ts
@@ -5,14 +5,13 @@ interface CardDataItem {
   urlLink: string;
 }
 
-// export const CardCarouselCardData = Object.freeze([
 export const CardCarouselCardData: ReadonlyArray<CardDataItem> = Object.freeze([
   {
     imagePath:
       "https://s3.us-west-1.amazonaws.com/dev.assets.fceja/images/bingo.png",
     title: "Bingo Card Game",
     description:
-      "Imatation of card game, where game is won by selecting a predetermined pattern.",
+      "Imitation of card game, where the game is won when a predetermined pattern is selected.",
     urlLink: "http://dev.frontend.bingo.s3-website-us-west-1.amazonaws.com",
     gitHubLink: "https://github.com/fceja/bingo_game",
   },
