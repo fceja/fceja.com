07c255cc1775db91bed46939cdf1fedaae62acb8
fceja <cisco_ceja@icloud.com>
2023-10-19 22:19:07 -0700

    move index.tsx to ts dir

---
+++ src/ts/index.tsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "@styles/Global.scss";
+import Root from "@components/Root";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
