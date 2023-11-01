f5b1bf585a2bf05069012f0f198505afc156d7a9
fceja <cisco_ceja@icloud.com>
2023-11-01 10:08:29 -0700

    include google maps card and adjust scss

--- src/ts/data/CardData.ts
+++ src/ts/data/CardData.ts
@@ -3,13 +3,45 @@ import { CardDataType } from "@appTypes/index";
 export const CardsData: Readonly<CardDataType> = Object.freeze([
   {
     cardDetails: {
+      title: "Google Map - Login",
+      description: "After logging in, an interactive Google Map is populated",
+      listSections: [
+        {
+          title: "Technologies",
+          listItems: ["TypeScript", "HTML", "SCSS"],
+        },
+        {
+          title: "Frameworks / Libraries / Modules",
+          listItems: ["Postgres", "React", "Axios", "Webpack", "Google Maps"],
+        },
+        {
+          title: "Cloud - AWS",
+          listItems: ["S3", "API Gateway", "Lambda", "RDS"],
+        },
+      ],
+    },
+    index: null,
+    webLinks: {
+      gitHubLink: "https://github.com/fceja/google_maps_app",
+      imageUrl:
+        "https://s3.us-west-1.amazonaws.com/dev.assets.fceja/images/google_maps.png",
+      urlLink:
+        "http://dev.client.google-maps.s3-website-us-west-1.amazonaws.com/",
+    },
+  },
+  {
+    cardDetails: {
       title: "Bingo Card Game",
       description:
         "Imitation of card game, where the game is won when a predetermined pattern is selected.",
       listSections: [
         {
-          title: "Technologies 1",
+          title: "Technologies",
           listItems: ["JavaScript", "HTML", "CSS"],
+        },
+        {
+          title: "Cloud - AWS",
+          listItems: ["S3"],
         },
       ],
     },
