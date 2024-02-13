a4739639e1b1df23af46fd2b06af207f6fd06c4e
fceja <88167774+fceja@users.noreply.github.com>
2024-02-13 11:51:49 -0800

    remove elems when animation complete

--- src/ts/components/greeting/Greeting.tsx
+++ src/ts/components/greeting/Greeting.tsx
@@ -1,37 +1,76 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { GreetingAnimation } from "./GreetingAnimation"
+import { onCompleteType } from "./GreetingTypes";
 
 const Greeting = () => {
+    const [isInitiated, setIsInitiated] = useState(false)
+    const line1Ref = useRef<HTMLHeadingElement>(null)
+    const line2Ref = useRef<HTMLHeadingElement>(null)
+    const line3Ref = useRef<HTMLHeadingElement>(null)
 
     useEffect(() => {
-        GreetingAnimation()
+        const onComplete: onCompleteType = () => {
+            setIsInitiated(true)
+        }
+        GreetingAnimation(onComplete)
     }, [])
 
-    return (
-        <>
-            <h1 className="ml11">
-                <span className="text-wrapper">
-                    <span className="line line1"></span>
-                    <span className="letters1">Hi.</span>
-                </span>
-            </h1>
-            <h1 className="ml12">
-                <span className="text-wrapper">
-                    <span className="line line2"></span>
-                    <span className="letters2">I'm Francisco.</span>
-                </span>
-            </h1>
-            <h1 className="ml13">
-                <span className="text-wrapper">
-                    <span className="line line3"></span>
-                    <span className="letters3">I <span id="heart">&#x2764;</span> code.</span>
-                </span>
-            </h1>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
-        </>
+    /**
+     * Once animation is initiated, we check if elem's exist in DOM.
+     * In order to let animation complete, we interval until
+     * all opacities are zero (not visible).
+     * Then, remove from DOM.
+    **/
+    useEffect(() => {
+        const htmlElem1 = line1Ref.current;
+        const htmlElem2 = line2Ref.current;
+        const htmlElem3 = line3Ref.current;
 
-    )
+        // check if elem's exist in DOM
+        if (htmlElem1 && htmlElem2 && htmlElem3) {
+            const intervalId = setInterval(() => {
+                // retrieve opacities
+                const opacity1 = window.getComputedStyle(htmlElem1).opacity;
+                const opacity2 = window.getComputedStyle(htmlElem2).opacity;
+                const opacity3 = window.getComputedStyle(htmlElem3).opacity;
+
+                // checks if all zero
+                if (!parseFloat(opacity1) && !parseFloat(opacity2) && !parseFloat(opacity3)) {
+                    // remove elems from DOM
+                    htmlElem1.remove();
+                    htmlElem2.remove();
+                    htmlElem3.remove();
+
+                    clearInterval(intervalId)
+                }
+            }, 750)
+        }
+    }, [isInitiated])
+
+    const animationHeadingElems = <>
+        <h1 ref={line1Ref} className="heading-1">
+            <span className="text-wrapper">
+                <span className="line line1"></span>
+                <span className="letters1">Hi.</span>
+            </span>
+        </h1>
+        <h1 ref={line2Ref} className="heading-2">
+            <span className="text-wrapper">
+                <span className="line line2"></span>
+                <span className="letters2">I'm Francisco.</span>
+            </span>
+        </h1>
+        <h1 ref={line3Ref} className="heading-3">
+            <span className="text-wrapper">
+                <span className="line line3"></span>
+                <span className="letters3">I <span id="heart">&#x2764;</span> code.</span>
+            </span>
+        </h1>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    </>
+
+    return (animationHeadingElems)
 }
 
 export default Greeting
--- src/ts/components/greeting/GreetingAnimation.ts
+++ src/ts/components/greeting/GreetingAnimation.ts
@@ -1,35 +1,36 @@
 import anime from 'animejs/lib/anime.es.js';
 
 import "@scss/components/GreetingAnimation.scss"
+import { onCompleteType } from "./GreetingTypes";
 
-export const GreetingAnimation = () => {
-    let textWrapper = document.querySelector('.ml11 .letters1');
+export const GreetingAnimation = (onComplete: onCompleteType) => {
+    let textWrapper = document.querySelector('.heading-1 .letters1');
     if (textWrapper && textWrapper.textContent) {
         textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w|\.|'|,)/g, "<span class='letter1'>$&</span>");
 
         anime.timeline({ loop: false })
             .add({
-                targets: '.ml11 .line1',
+                targets: '.heading-1 .line1',
                 scaleY: [0, 1],
                 opacity: [0.5, 1],
                 easing: "easeOutExpo",
                 duration: 700
             })
             .add({
-                targets: '.ml11 .line1',
+                targets: '.heading-1 .line1',
                 translateX: [0, textWrapper.getBoundingClientRect().width + 10],
                 easing: "easeOutExpo",
                 duration: 700,
                 delay: 300
             }).add({
-                targets: '.ml11 .letter1',
+                targets: '.heading-1 .letter1',
                 opacity: [0, 1],
                 easing: "easeOutExpo",
                 duration: 600,
                 offset: '-=775',
-                delay: (el, i) => 34 * (i + 1)
+                delay: (_, i) => 34 * (i + 1)
             }).add({
-                targets: '.ml11',
+                targets: '.heading-1',
                 opacity: 0,
                 duration: 1000,
                 easing: "easeOutExpo",
@@ -37,32 +38,33 @@ export const GreetingAnimation = () => {
             });
     }
 
-    let textWrapper2 = document.querySelector('.ml12 .letters2');
+    let textWrapper2 = document.querySelector('.heading-2 .letters2');
     if (textWrapper2 && textWrapper2.textContent) {
         textWrapper2.innerHTML = textWrapper2.textContent.replace(/([^\x00-\x80]|\w|\.|'|,)/g, "<span class='letter2'>$&</span>");
         anime.timeline({ loop: false })
             .add({
-                targets: '.ml12 .line2',
+                targets: '.heading-2 .line2',
                 scaleY: [0, 1],
                 opacity: [0.5, 1],
                 easing: "easeOutExpo",
                 duration: 700
             })
             .add({
-                targets: '.ml12 .line2',
+                targets: '.heading-2 .line2',
                 translateX: [0, textWrapper2.getBoundingClientRect().width + 10],
                 easing: "easeOutExpo",
                 duration: 700,
                 delay: 1500
             }).add({
-                targets: '.ml12 .letter2',
+                targets: '.heading-2 .letter2',
                 opacity: [0, 1],
                 easing: "easeOutExpo",
                 duration: 600,
                 offset: '-=775',
-                delay: (el, i) => 34 * (i + 1)
+                // delay: (el, i) => 34 * (i + 1)
+                delay: (_, i) => 34 * (i + 1)
             }).add({
-                targets: '.ml12',
+                targets: '.heading-2',
                 opacity: 0,
                 duration: 1000,
                 easing: "easeOutExpo",
@@ -70,7 +72,7 @@ export const GreetingAnimation = () => {
             });
     }
 
-    let textWrapper3 = document.querySelector('.ml13 .letters3');
+    let textWrapper3 = document.querySelector('.heading-3 .letters3');
     if (textWrapper3 && textWrapper3.textContent) {
         textWrapper3.innerHTML = textWrapper3.textContent.replace(/([^\x00-\x80]|\w|\.|'|,)/g, "<span class='letter3'>$&</span>");
 
@@ -85,31 +87,33 @@ export const GreetingAnimation = () => {
 
         anime.timeline({ loop: false })
             .add({
-                targets: '.ml13 .line3',
+                targets: '.heading-3 .line3',
                 scaleY: [0, 1],
                 opacity: [0.5, 1],
                 easing: "easeOutExpo",
                 duration: 700
             })
             .add({
-                targets: '.ml13 .line3',
+                targets: '.heading-3 .line3',
                 translateX: [0, textWrapper3.getBoundingClientRect().width + 10],
                 easing: "easeOutExpo",
                 duration: 700,
-                delay: 3200
+                delay: 3400
             }).add({
-                targets: '.ml13 .letter3',
+                targets: '.heading-3 .letter3',
                 opacity: [0, 1],
                 easing: "easeOutExpo",
                 duration: 600,
                 offset: '-=775',
-                delay: (el, i) => 34 * (i + 1)
+                delay: (_, i) => 34 * (i + 1)
             }).add({
-                targets: '.ml13',
+                targets: '.heading-3',
                 opacity: 0,
                 duration: 1000,
                 easing: "easeOutExpo",
-                delay: 2200
+                delay: 1800
             });
     }
+
+    onComplete()
 }
