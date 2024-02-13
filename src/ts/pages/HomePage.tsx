017b48092ba3465dcf5ab31f49c23b71bd6c1743
fceja <88167774+fceja@users.noreply.github.com>
2024-02-12 19:25:16 -0800

    add api fetch

--- src/ts/pages/HomePage.tsx
+++ src/ts/pages/HomePage.tsx
@@ -4,15 +4,58 @@ import "@scss/pages/HomePage.scss";
 import Loading from "@components/Loading"
 
 
+type ResponseData = {
+    status: string,
+    articles: [{
+        source: { name: string },
+        title: string
+    }],
+
+}
+
+const apiUrl = ""
+
 const HomePage = () => {
     const [isLoading, setIsLoading] = useState(false)
+    const [responseData, setResponseData] = useState<ResponseData | null>(null)
+    const [headlines, setHeadlines] = useState<string[]>([])
+
+    useEffect(() => {
+        if (!responseData || responseData.status !== 'ok') return;
+
+        responseData.articles.forEach((article) => {
+            if (article.title.includes('Removed')) {
+                return // continue
+
+            }
+            setHeadlines(headlines => [...headlines, `${article.source.name} - ${article.title}`])
+        })
+
+    }, [responseData])
+
+    useEffect(() => {
+        console.log(`headlines 3-> ${headlines}`)
+
+    }, [headlines])
 
     useEffect(() => {
         setIsLoading(true)
-        setTimeout(() => {
-            setIsLoading(false)
 
-        }, 2000)
+        // fetch data
+        const fetchData = async () => {
+            try {
+                const response = await fetch(apiUrl)
+                const data = await response.json()
+                setResponseData(data)
+
+            } catch (error) {
+                console.error(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        fetchData()
+
     }, [])
 
     return (
