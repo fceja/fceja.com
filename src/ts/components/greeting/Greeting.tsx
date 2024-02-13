93d2773e66d1379d4c663eecde39ad05ce7b1d5b
fceja <88167774+fceja@users.noreply.github.com>
2024-02-12 23:05:17 -0800

    add second line animation

--- src/ts/components/greeting/Greeting.tsx
+++ src/ts/components/greeting/Greeting.tsx
@@ -13,10 +13,15 @@ const Greeting = () => {
             <h1 className="ml11">
                 <span className="text-wrapper">
                     <span className="line line1"></span>
-                    <span className="letters">Hello Goodbye</span>
+                    <span className="letters1">Hi.</span>
                 </span>
             </h1>
-
+            <h1 className="ml12">
+                <span className="text-wrapper">
+                    <span className="line line2"></span>
+                    <span className="letters2">I'm Francisco.</span>
+                </span>
+            </h1>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
         </>
 
--- src/ts/components/greeting/GreetingAnimation.ts
+++ src/ts/components/greeting/GreetingAnimation.ts
@@ -3,26 +3,26 @@ import anime from 'animejs/lib/anime.es.js';
 import "@scss/components/GreetingAnimation.scss"
 
 export const GreetingAnimation = () => {
-    var textWrapper = document.querySelector('.ml11 .letters');
+    var textWrapper = document.querySelector('.ml11 .letters1');
     if (textWrapper && textWrapper.textContent) {
-        textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+        textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w|\.|'|,)/g, "<span class='letter1'>$&</span>");
 
-        anime.timeline({ loop: true })
+        anime.timeline({ loop: false })
             .add({
-                targets: '.ml11 .line',
+                targets: '.ml11 .line1',
                 scaleY: [0, 1],
                 opacity: [0.5, 1],
                 easing: "easeOutExpo",
                 duration: 700
             })
             .add({
-                targets: '.ml11 .line',
+                targets: '.ml11 .line1',
                 translateX: [0, textWrapper.getBoundingClientRect().width + 10],
                 easing: "easeOutExpo",
                 duration: 700,
-                delay: 100
+                delay: 1200
             }).add({
-                targets: '.ml11 .letter',
+                targets: '.ml11 .letter1',
                 opacity: [0, 1],
                 easing: "easeOutExpo",
                 duration: 600,
@@ -33,7 +33,40 @@ export const GreetingAnimation = () => {
                 opacity: 0,
                 duration: 1000,
                 easing: "easeOutExpo",
-                delay: 1000
+                delay: 2000
+            });
+    }
+
+    var textWrapper2 = document.querySelector('.ml12 .letters2');
+    if (textWrapper2 && textWrapper2.textContent) {
+        textWrapper2.innerHTML = textWrapper2.textContent.replace(/([^\x00-\x80]|\w|\.|'|,)/g, "<span class='letter2'>$&</span>");
+        anime.timeline({ loop: false })
+            .add({
+                targets: '.ml12 .line2',
+                scaleY: [0, 1],
+                opacity: [0.5, 1],
+                easing: "easeOutExpo",
+                duration: 700
+            })
+            .add({
+                targets: '.ml12 .line2',
+                translateX: [0, textWrapper2.getBoundingClientRect().width + 10],
+                easing: "easeOutExpo",
+                duration: 700,
+                delay: 2000
+            }).add({
+                targets: '.ml12 .letter2',
+                opacity: [0, 1],
+                easing: "easeOutExpo",
+                duration: 600,
+                offset: '-=775',
+                delay: (el, i) => 34 * (i + 1)
+            }).add({
+                targets: '.ml12',
+                opacity: 0,
+                duration: 1000,
+                easing: "easeOutExpo",
+                delay: 2200
             });
     }
 }
