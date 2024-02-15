afc355a52fd56e9253958deb985a2a924bb3948a
fceja <88167774+fceja@users.noreply.github.com>
2024-02-15 13:50:34 -0800

    pass in content to subSection comp

--- src/ts/components/subSection/SubSection.tsx
+++ src/ts/components/subSection/SubSection.tsx
@@ -1,19 +1,21 @@
 import "@scss/components/SubSection.scss";
-import { LinksData } from "@data/linkData/LinkData";
 
-const subTitle = "Languages / Libraries / Frameworks";
-const anchorTags = LinksData.languageLibraryFramework.map((elem, i) => {
-  return (
-    <a key={`${i}-a-img`} href={elem.href} target="_blank" rel="noreferrer">
-      <img src={elem.img.src} alt={elem.img.alt} width="60" height="60" />
-    </a>
-  );
-});
+import { SubSectionContentProps } from "@content/SubSectionContent";
 
-const SubSection = () => {
+type SubSectionProps = SubSectionContentProps;
+
+const SubSection = (props: SubSectionProps) => {
+  const anchorTags = props.anchorTagData.map((elem, i) => {
+    return (
+      <a key={`${i}-a-img`} href={elem.href} target="_blank" rel="noreferrer">
+        <img src={elem.img.src} alt={elem.img.alt} width="60" height="60" />
+      </a>
+    );
+  });
+
   return (
     <section className="sub-section">
-      <h2 className="sub-heading">{subTitle}</h2>
+      <h2 className="sub-heading">{props.title}</h2>
       <p className="p-contents">{anchorTags}</p>
     </section>
   );
--- src/ts/pages/HomePage.tsx
+++ src/ts/pages/HomePage.tsx
@@ -5,16 +5,21 @@ import Loading from "@common/components/Loading";
 import Greeting from "@components/greeting/Greeting";
 import Intro from "@components/intro/Intro";
 import SubSection from "@components/subSection/SubSection";
+import { SubSectionContent } from "@content/SubSectionContent";
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
+
+  const subSectionElems = SubSectionContent.map((elem, index) => {
+    return <SubSection key={index} {...elem} />;
+  });
 
   return (
     <div className="home-page-container">
       {isLoading && <Loading />}
       <Greeting />
       <Intro />
-      <SubSection />
+      {subSectionElems}
     </div>
   );
 };
