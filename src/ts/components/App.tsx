7ab5926d7b37bff546ac5819f41f86a77e3f462b
fceja <88167774+fceja@users.noreply.github.com>
2024-05-20 20:25:36 -0700

    remove bootstrap container

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,4 +1,3 @@
-import Container from "react-bootstrap/Container";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { PAGE_PATHS } from "@common/Constants";
@@ -7,13 +6,11 @@ import ProjectsPage from "@pages/ProjectsPage";
 
 const App = () => {
   return (
-    <Container fluid className="app-container p-0" id="app">
-      <Routes>
-        <Route path="/*" element={<Navigate to={`${PAGE_PATHS.HOME}`} />} />
-        <Route path={PAGE_PATHS.HOME} element={<HomePage />} />
-        <Route path={PAGE_PATHS.PROJECTS} element={<ProjectsPage />} />
-      </Routes>
-    </Container>
+    <Routes>
+      <Route path="/*" element={<Navigate to={`${PAGE_PATHS.HOME}`} />} />
+      <Route path={PAGE_PATHS.HOME} element={<HomePage />} />
+      <Route path={PAGE_PATHS.PROJECTS} element={<ProjectsPage />} />
+    </Routes>
   );
 };
 
